c6f9675b6bec0a0f80e9fb6116db92c76f2f94bd
Peefy <xpf6677@163.com>
2019-07-06 12:23:40 +0800

    Update

--- src/dac.cpp
+++ src/dac.cpp
@@ -6,6 +6,9 @@ static uint8 __isDACInit = 0;
 static void duguDACInit(){
     if (__isDACInit == 1)
         return;   
+    rcc_clk_disable(RCC_USART2);
+    rcc_clk_disable(RCC_SPI1);
+    rcc_clk_disable(RCC_ADC1);
     dac_init(DAC, DAC_CH1 | DAC_CH2);
     __isDACInit = 1;
 }
