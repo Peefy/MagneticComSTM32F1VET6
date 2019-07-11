e1e991fda9b9749a011940e3a4176ed541888b94
Peefy <xpf6677@163.com>
2019-07-11 21:18:27 +0800

    Update

---
+++ src/ad7055.h
@@ -0,0 +1,18 @@
+
+#ifndef _AD7055_H_
+#define _AD7055_H_
+
+#include <Arduino.h>
+
+void bsp_InitTM7705(void);
+void TM7705_CalibSelf(uint8_t _ch);
+void TM7705_SytemCalibZero(uint8_t _ch);
+void TM7705_SytemCalibFull(uint8_t _ch);
+uint16_t TM7705_ReadAdc(uint8_t _ch);
+
+void TM7705_WriteReg(uint8_t _RegID, uint32_t _RegValue);
+uint32_t TM7705_ReadReg(uint8_t _RegID);
+
+void Test_ADF7705();
+
+#endif
---
+++ src/ad9767.h
@@ -0,0 +1,48 @@
+
+#ifndef _DAC_H_
+#define _DAC_H_
+
+#include <Arduino.h>
+
+#define CHIP_NAME "DAC9767"
+
+// DA1相关引脚(J3)                   
+#define DA1_DATA0     PD0              
+#define DA1_DATA1     PD1  
+#define DA1_DATA2     PD2  
+#define DA1_DATA3     PD3  
+#define DA1_DATA4     PD4  
+#define DA1_DATA5     PD5  
+#define DA1_DATA6     PD6  
+#define DA1_DATA7     PD7  
+#define DA1_DATA8     PD8  
+#define DA1_DATA9     PD9  
+#define DA1_DATA10    PD10  
+#define DA1_DATA11    PD11  
+#define DA1_DATA12    PD12  
+#define DA1_DATA13    PD13  
+#define DA1_CLK       PD14
+#define DA1_WRT       PD15                
+
+// DA2相关引脚(J3)                   
+#define DA2_DATA0     PE0              
+#define DA2_DATA1     PE1  
+#define DA2_DATA2     PE2  
+#define DA2_DATA3     PE3  
+#define DA2_DATA4     PE4  
+#define DA2_DATA5     PE5  
+#define DA2_DATA6     PE6  
+#define DA2_DATA7     PE7  
+#define DA2_DATA8     PE8  
+#define DA2_DATA9     PE9  
+#define DA2_DATA10    PE10  
+#define DA2_DATA11    PE11  
+#define DA2_DATA12    PE12  
+#define DA2_DATA13    PE13  
+#define DA2_CLK       PE14
+#define DA2_WRT       PE15                       
+
+void DAC9767_Init();
+void DAC9767_WriteData1(int16_t val);
+
+#endif 
